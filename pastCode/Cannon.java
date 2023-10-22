4cb643c4892f498fd2f474455829742755fd7794
aaron <48113149+karmette@users.noreply.github.com>
2023-10-22 00:51:26 -0500

    Create Cannon.java

---
+++ pastCode/Cannon.java
@@ -0,0 +1,20 @@
+package EulerJava;
+import java.util.Scanner;
+public class Cannon {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int tests = s.nextInt();
+        for(int i = 0; i < tests; i++){
+            int cannons = s.nextInt();
+            int max = s.nextInt();
+            int needed = 0;
+            for(int c = 0; c<cannons; c++){
+                needed += max-s.nextInt();
+            }
+            double cost = s.nextInt();
+            double hasStock = s.nextInt();
+            double stock = Math.ceil(needed/hasStock);
+            System.out.println((int)(cost*stock) + " coins");
+        }
+    }
+}
