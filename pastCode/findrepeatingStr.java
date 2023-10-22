d7040c2246035edd7e30c793bff12dac3fd9a25a
Dani5974 <138187291+Dani5974@users.noreply.github.com>
2023-10-22 01:08:15 -0500

    Update and rename repeatingStr.java to findrepeatingStr.java

--- pastCode/findrepeatingStr.java
+++ pastCode/findrepeatingStr.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class repeatingStr {
+public class findrepeatingStr {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         String test=sc.next();
