d431877949434558a8cee672ed6f070468f61152
aaron <48113149+karmette@users.noreply.github.com>
2023-10-22 00:53:29 -0500

    Add files via upload

---
+++ pastCode/waves.java
@@ -0,0 +1,23 @@
+package EulerJava;
+import java.util.Scanner;
+public class waves {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int tests = s.nextInt();
+        for(int i = 0; i < tests; i++){
+            int waves = s.nextInt();
+            int[] waveHeights = new int[waves];
+            int peaks = 0;
+            for(int w = 0; w < waves; w++){
+                int wH = s.nextInt();
+                waveHeights[w] = wH;
+            }
+            for(int l = 1; l < waves-1; l++){
+                if((waveHeights[l-1] < waveHeights[l]) && (waveHeights[l + 1] < waveHeights[l])){
+                    peaks++;
+                }
+            }
+            System.out.println(peaks);
+        }
+    }
+}
