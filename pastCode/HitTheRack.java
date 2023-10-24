ed21a2dda0c7a730c5a4eda61f2262c2654c8829
karmette <yaya.ny@proton.me>
2023-10-23 22:13:20 -0500

    add unfinished shitty code

---
+++ pastCode/HitTheRack.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class HitTheRack {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int tests = s.nextInt();
+        for(int i = 0; i<tests; i++){
+            String time = s.nextLine();
+            String[] times = new String[4];
+            
+        }
+    }
+}
---
+++ pastCode/MessageBottle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class MessageBottle {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String correct = s.nextLine();
+        int tests = s.nextInt();
+        int[] results = new int[tests-1];
+        for(int i = 0; i<tests; i++){
+            
+            String testString = s.nextLine();
+            String[] testArray = new String[correct.length()-1];
+
+            int cursor = 0;
+
+            while(cursor < testArray.length){
+                String testChar = testString.substring(cursor, cursor+1);
+                if(testChar.equals("#")){
+                    
+                    cursor--;
+
+                }else{
+                    testArray[cursor] = testChar;
+                }
+            }
+            
+        }
+    }
+}
